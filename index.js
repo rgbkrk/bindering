319414045248d39e8dfc9a6e5d74aa6587e3e5d7
Kyle Kelley <rgbkrk@gmail.com>
2017-11-28 10:51:55 -0800

    setup a full kernel connect demo

--- index.js
+++ index.js
@@ -1,41 +1,144 @@
 const { binder } = require("rx-binder");
 
 const { kernels } = require("rx-jupyter");
+
 const { filter, map, switchMap, tap } = require("rxjs/operators");
 
-const EventSource = require("eventsource");
+const uuid = require("uuid");
+
+/** Forced polyfills for node **/
 const { XMLHttpRequest } = require("xmlhttprequest");
-
+const WebSocket = require("ws");
 global.XMLHttpRequest = XMLHttpRequest;
+global.WebSocket = WebSocket;
 
-connection = kernels.connect(x.serverConfig, x.kernel.response.id);
+async function sleep(t = 1000) {
+  return new Promise(resolve => setTimeout(resolve, t));
+}
 
-function kernelMe(kernelName = "python3") {
-  return binder({ repo: "jupyterlab/jupyterlab" }, EventSource).pipe(
-    tap(msg => console.log(msg)),
-    filter(m => m.phase === "ready"),
-    map(msg => {
-      const serverConfig = {
-        endpoint: msg.url.replace(/\/\s*$/, ""),
-        uri: "/",
-        token: msg.token
-      };
-      return serverConfig;
-    }),
-    switchMap(serverConfig => {
-      return kernels.start(serverConfig, kernelName, "").pipe(
-        map(aj => {
-          return {
-            serverConfig,
-            kernel: aj.response,
-            kernelConnection: kernels.connect(serverConfig, aj.response.id)
-          };
-        })
-      );
+async function serverMe(repo = "jupyterlab/jupyterlab") {
+  return binder({ repo })
+    .pipe(
+      tap(msg => {
+        if (msg.phase === "building") {
+          console.log(msg.message);
+        } else {
+          console.log(msg);
+        }
+      }),
+      filter(msg => msg.phase === "ready"),
+      map(msg => {
+        const serverConfig = {
+          endpoint: msg.url.replace(/\/\s*$/, ""),
+          uri: "/",
+          token: msg.token
+        };
+        return serverConfig;
+      })
+    )
+    .toPromise();
+}
+
+async function kernelMe(serverConfig, kernelName = "python3") {
+  const session = uuid();
+
+  return kernels
+    .start(serverConfig, kernelName, "")
+    .pipe(
+      map(aj => {
+        return Object.assign({}, aj.response, {
+          session: session,
+          channels: kernels.connect(serverConfig, aj.response.id, session)
+        });
+      })
+    )
+    .toPromise();
+}
+
+async function main() {
+  const serverConfig = await serverMe("nteract/vdom");
+  const kernel = await kernelMe(serverConfig);
+
+  var sub = kernel.channels.subscribe(
+    x => console.log("kernel message", x),
+    err => console.error("kernel error", err),
+    () => console.log("kernel complete!")
+  );
+
+  kernel.channels.next(
+    JSON.stringify({
+      header: {
+        msg_id: uuid(),
+        username: "username",
+        session: kernel.session,
+        date: new Date().toISOString(),
+        msg_type: "kernel_info_request",
+        version: "5.2"
+      },
+      channel: "shell",
+      parent_header: {},
+      metadata: {},
+      content: {},
+      buffers: []
     })
   );
+
+  console.log(
+    "sleeping for a second, though we really just need to wait for status: idle"
+  );
+
+  await sleep(60);
+
+  kernel.channels.next(
+    JSON.stringify({
+      header: {
+        msg_id: uuid(),
+        username: "username",
+        session: kernel.session,
+        date: new Date().toISOString(),
+        msg_type: "kernel_info_request",
+        version: "5.2"
+      },
+      channel: "shell",
+      parent_header: {},
+      metadata: {},
+      content: {},
+      buffers: []
+    })
+  );
+
+  console.log("Sleeping, mostly just for fun");
+  await sleep(60);
+
+  kernel.channels.next(
+    JSON.stringify({
+      header: {
+        msg_id: uuid(),
+        username: "username",
+        session: kernel.session,
+        date: new Date().toISOString(),
+        msg_type: "shutdown_request",
+        version: "5.2"
+      },
+      channel: "shell",
+      parent_header: {},
+      metadata: {},
+      content: {},
+      buffers: []
+    })
+  );
+
+  console.log("Sleeping, mostly just for fun");
+  await sleep(60);
+
+  kernel.channels.complete();
+
+  console.log("Killing kernel");
+  kernels.kill(serverConfig, kernel.id);
 }
 
 module.exports = {
-  kernelMe
+  kernelMe,
+  serverMe,
+  main
 };
---
+++ run.js
@@ -0,0 +1,3 @@
+const { main } = require(".");
+
+main().then(console.log);
